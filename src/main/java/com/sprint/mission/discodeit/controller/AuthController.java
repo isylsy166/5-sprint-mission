2abcafea6d167de690b7ab57de589e0397e08fb7
Soyeon <isylsy166@gmail.com>
2025-09-04 11:23:21 +0900

    refactor: Auth, Channel Dto로 응답

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
+import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.LoginRequest;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController implements AuthApi {
 
   private final AuthService authService;
+  private final UserMapper userMapper;
+  private final UserStatusService userStatusService;
 
   @PostMapping(path = "login")
-  public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
+  public ResponseEntity<UserDto> login(@RequestBody LoginRequest loginRequest) {
     User user = authService.login(loginRequest);
+    UserStatus userStatus = userStatusService.findByUserId(user.getId());
     return ResponseEntity
         .status(HttpStatus.OK)
-        .body(user);
+        .body(userMapper.toDto(user, userStatus));
   }
 }
